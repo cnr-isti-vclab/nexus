e2777d1ca453878ed23c569c5237d4d5c53fe3d6
ponchio <ponchio@gmail.com>
2017-03-13 00:45:27 +0100

    design of the tex pyramid storage.

--- src/nxsbuild/texpyramid.cpp
+++ src/nxsbuild/texpyramid.cpp
@@ -3,14 +3,100 @@
 using namespace nx;
 using namespace std;
 
-bool addTextures(std::vector<QString> &filenames) {
-	pyramids.resize(filenames.size());
-	for(int i = 0; i < pyramids.size(); i++) {
-		TexPyramid &py = pyramids[i];
-		py.init(filenames, cache);
+bool TexLevel::init(int t, TexCollection *c, QImageReader &reader) {
+	tex = t;
+	level = 0;
+	collection = c;
+	int side = collection->side;
+	width = reader.size().width();
+	height = reader.size().height();
+
+	tilew = 1 + floor(width/side);
+	tileh = 1 + floor(height/side);
+	offsets.resize(tilew*tileh + 1);
+	for(int y = 0; y < tileh; y++) {
+		for(int x = 0; x < tilew; x++) {
+			int sx = x*side;
+			int wx = (sx + side > width)? width - sx : side;
+			int sy = y*side;
+			int wy = (sy + side > width)? width - sy : side;
+			reader.setClipRect(QRect(sx, sy, wx, wy));
+			QImage img = reader.read();
+			if(img.isNull()) return false;
+			offsets[y*tilew + x] = collection->storage.pos();
+			img.save(&(collection->storage), "jpg", collection->quality);
+		}
+	}
+	offsets.back() = collection->storage.pos();
+	return true;
+}
+
+QImage TexLevel::read(QRect region) {
+	int side = collection->side;
+
+	int sx = region.x()/side;
+	int sy = region.y()/side;
+	int ex = (region.x() + region.width() - 1)/side; //last pixel needed
+	int ey = (region.y() + region.height() - 1)/side;
+
+	QImage image(region.size(), QImage::Format_RGB32);
+
+	for(int y = sy; y <= ey; y++) {
+		for(int x = sx; x <= ex; x++) {
+			int index = sx + sy * tilew;
+
+			auto it = collection->cache.find(TexCollection::Index(tex, level, index));
+			if(!it) {
+				uint64_t size = offsets[index+1] - offsets[index];
+				uchar *data = file.map(offsets[index], size);
+				QImage img;
+				img.loadFromData(data, size);
+				//copy into image
+				//store into cache.
+				//remove excess data from cache
+			}
+		}
 	}
 }
 
-QImage read(int tex, int level, QRect region) {
+bool TexPyramid::init(int tex, TexCollection *c, const QString &file) {
+	collection = c;
+	QImageReader reader(file);
+	if(!reader.canRead()) return false;
 
+	//create level zero.
+	levels.resize(1);
+	TexLevel &level = levels.back();
+	level.init(tex, collection, reader);
 }
+
+QImage TexPyramid::read(int level, QRect region) {
+	return levels[level].read(tex, level, region);
+}
+
+
+
+bool TexCollection::addTextures(std::vector<QString> &filenames) {
+	pyramids.resize(filenames.size());
+	for(int i = 0; i < pyramids.size(); i++) {
+		TexPyramid &py = pyramids[i];
+		py.quality = quality;
+		bool ok = py.init(i, this, filenames[i]);
+		if(!ok) return false;
+	}
+}
+
+QImage TexCollection::read(int tex, int level, QRect region) {
+	return pyramids[tex].read(level, region);
+}
+
+void TexCollection::flush(int level) {
+	for (auto it = cache.cbegin(); it != cache.cend();) {
+		if (*it.level == level)
+			it = m.erase(it);
+		else
+			++it;
+	}
+}
+
+
--- src/nxsbuild/texpyramid.h
+++ src/nxsbuild/texpyramid.h
@@ -9,41 +9,64 @@
 
 namespace nx {
 
+class TexCollection;
+
 //split and store in temporary files large textures for better access
 class TexLevel {
-	const int side = 1024;
+public:
+	TexCollection *collection;
+	int tex, level;
 	int width, height;
 	int tilew, tileh;              //how many tiles to make up side.
-	std::vector<uint32_t> offsets; //where each tile starts in the cache.
+	std::vector<qint64> offsets; //where each tile starts in the cache.
+
+	bool init(int tex, TexCollection *c, QImageReader &reader);
+	QImage read(QRect region);
 };
 
 //manage a pyramid of splitted textured
 class TexPyramid {
 public:
+	TexCollection *collection;
 	std::vector<TexLevel> levels;
+
+	bool init(int tex, TexCollection *c, const QString &file);
+	QImage read(int level, QRect region);
 };
 
 //Collection of tex pyramids, level 0 is biggest one (bottom).
 class TexCollection {
 public:
+	const int side = 1024;
 	std::vector<TexPyramid> pyramids;
 	float scale;
+	int quality;
 
-	TexCollection(): scale(M_SQRT1_2) {}
+	TexCollection(): scale(M_SQRT1_2), quality(92) {}
 
 	bool addTextures(std::vector<QString> &filenames);
 	QImage read(int tex, int level, QRect region);
 
 	int width(int tex, int level) { return pyramids[tex].levels[level].width; }
 	int height(int tex, int level) { return pyramids[tex].levels[level].height; }
+	void flush(int level);
 
-protected:
 
 	struct Index {
 		int tex;
 		int level;
 		int index;
 		int access; //record last access time.
+		Index() {}
+		Index(int t, int l, int i, int a = 0): tex(level), level(l), index(i), access(a) {}
+		bool operator<(const Index &i) {
+			if(tex = i.tex) {
+				if(level == i.level)
+					return index < i.index;
+				return level < i.level;
+			}
+			return tex < i.tex;
+		}
 	};
 
 	std::map<Index, QImage> cache;
