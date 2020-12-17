bc064aa7f20d517eb8e802aaf3f536e64f75a9e3
ponchio <ponchio@gmail.com>
2020-12-17 23:25:56 +0100

    added minification for lib, cleanup of npm lib.

--- nexus3d/rollup.config.js
+++ nexus3d/rollup.config.js
@@ -1,13 +1,11 @@
-function header() {
+import { terser } from "rollup-plugin-terser";
 
+function header() {
 	return {
 
 		renderChunk( code ) {
-
 			return "// vcg.isti.cnr.it/nexus/license\n" + code;
-
 		}
-
 	};
 
 }
@@ -15,12 +13,19 @@ function header() {
 export default [
 	{
 		input: './Nexus3D.js',
-		output: {
+		output: [{
 			format: 'umd',
 			name: 'Nexus3D',
 			file: 'build/nexus3D.min.js',
+			plugins: [terser()],
 			globals: { three: 'THREE' }
 		},
+		{
+			format: 'umd',
+			name: 'Nexus3D',
+			file: 'build/nexus3D.js',
+			globals: { three: 'THREE' }
+		}],
 		external: [ 'three' ]
 	}
 	
